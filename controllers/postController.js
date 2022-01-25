5387eec23c0228fdf3442d6d226ee71cfa5939da
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-25 10:51:41 -0500

    Run npm init / Add index.js connection

---
+++ controllers/postController.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const Post = require("../db/models/Post");
+
+const router = express.Router();
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const postController = require("./controllers/postController");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Redirect
+app.get("/", (req, res) => {
+	res.redirect("/posts");
+});
+
+// Controllers
+// Forward all requests to localhost:3111/posts to the post controller
+app.use("/posts", postController);
+
+app.listen(3111, () => {
+	console.log("Let's get posting...🐧");
+});
