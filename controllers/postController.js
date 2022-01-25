d416c276450c126d5d7cb5dd296e600cc68c2926
Andrew Kal <andrewkalgit@gmail.com>
2022-01-25 12:28:34 -0500

    get all posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,21 @@
-const express = require("express");
-const Post = require("../db/models/Post");
+//Require the express module
+const express = require('express');
 
+//Import the post model
+const Post = require('../db/models/Post');
+
+//Instantiate a router
 const router = express.Router();
+
+//Get all posts
+router.get('/', async (req, res, next) => {
+	try {
+		const posts = await Post.find({});
+
+		res.json(posts);
+	} catch (error) {
+		next(error);
+	}
+});
 
 module.exports = router;
